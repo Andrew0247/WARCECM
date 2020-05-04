b0c2994e9286dabc0d8cd9ae2c625ae79fdb7f95
Andrew0247 <62215737+Andrew0247@users.noreply.github.com>
2020-05-03 23:42:09 -0500

    Add files via upload

---
+++ RegContrac/REGEmpleados/admin.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+from REGEmpleados.models import Empleado,Contratacion,Educacion,Experiencia_Laboral,Documentos_Soporte,Valoracion
+
+# Register your models here.
+class Media:
+    js = (
+        '//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js',  # jquery
+        #'js/admin.js',       # project static folder
+    )
+
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display=("nom_empleado", "ape_empleado", "cedu_empleado", "email_empleado", "estado_empleado")
+    search_fields=("nom_empleado", "ape_empleado", "estado_empleado")
+    list_filter=('estado_empleado','cedu_empleado')
+
+class ContratacionAdmin(admin.ModelAdmin):
+    list_display=("cargo_empleado","tipo_contrato","contrato_doc", "fech_inicio_contrato", "fech_fin_contrato",)
+    search_fields=("desc_cargo_empleado","tipo_contrato",)
+    date_hierarchy="fech_inicio_contrato"
+    list_filter = ('fech_fin_contrato','tipo_contrato')
+
+class EducacionAdmin(admin.ModelAdmin):
+    list_display=("nom_instituto", "niv_educativo", "deta_educacion", "cursa_educacion","fech_inicio_edu","fech_fin_edu",)
+    search_fields=("nom_instituto", "niv_educacion", "cursa_educacion")
+    date_hierarchy="fech_inicio_edu"
+    list_filter=('fech_fin_edu','niv_educativo')
+
+class ExperienciaLaboralAdmin(admin.ModelAdmin):
+    list_display=("nom_empresa", "cargo_experiencia", "dura_experiencia","certificacion_labo","fech_inicio_expe","fech_fin_expe")
+    search_fields=("nom_empresa","dura_experiencia")
+    date_hierarchy="fech_inicio_expe"
+    list_filter=('dura_experiencia','fech_fin_expe','fech_inicio_expe')
+
+class Documentos_SoporteAdmin(admin.ModelAdmin):
+    list_display=("tipo_documento","documento_soporte",)
+    search_fields=("tipo_documento",)
+    list_filter=('tipo_documento',)
+
+class ValoracionAdmin(admin.ModelAdmin):
+    list_display=("valoracion", "observacion")
+    search_fields=("valoracion",)
+    list_filter=('valoracion',)
+
+admin.site.register(Empleado, EmpleadoAdmin)
+admin.site.register(Contratacion, ContratacionAdmin)
+admin.site.register(Educacion, EducacionAdmin)
+admin.site.register(Experiencia_Laboral, ExperienciaLaboralAdmin)
+admin.site.register(Documentos_Soporte, Documentos_SoporteAdmin)
+admin.site.register(Valoracion, ValoracionAdmin)
---
+++ RegContrac/REGEmpleados/forms.py
@@ -0,0 +1,98 @@
+from django import forms
+from django.forms import ModelForm
+from bootstrap_modal_forms.forms import BSModalForm # Se intalo bootstrap_modal_forms con django-bootstrap-modal-forms
+from REGEmpleados.models import Empleado, Contratacion, Educacion, Experiencia_Laboral, Documentos_Soporte, Valoracion
+
+class EmpleadoForm(ModelForm):
+    nom_empleado = forms.CharField(label="Nombre", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre'}))
+    ape_empleado = forms.CharField(label="Apellido", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Apellido'}))
+    cedu_empleado = forms.CharField(label="Cedula", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Cedula'}))
+    celu_empleado = forms.CharField(label="Celular", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Celular'}))
+    email_empleado = forms.EmailField(label="Email", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['estado_empleado'].widget.attrs.update({'class':'custom-control-input'})
+
+    class Meta:
+        model = Empleado
+        fields = ['nom_empleado', 'ape_empleado', 'cedu_empleado', 'celu_empleado', 'email_empleado', 'estado_empleado']
+
+class Empleado2Form(BSModalForm):
+    nom_empleado = forms.CharField(label="Nombre", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre'}))
+    ape_empleado = forms.CharField(label="Apellido", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Apellido'}))
+    cedu_empleado = forms.CharField(label="Cedula", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Cedula'}))
+    celu_empleado = forms.CharField(label="Celular", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Celular'}))
+    email_empleado = forms.EmailField(label="Email", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['estado_empleado'].widget.attrs.update({'class':'custom-control-input'})
+
+    class Meta:
+        model = Empleado
+        fields = ['nom_empleado', 'ape_empleado', 'cedu_empleado', 'celu_empleado', 'email_empleado']
+
+class ContratacionForm(BSModalForm):
+    class Meta:
+        model = Contratacion
+        fields = ['empleado','cargo_empleado','tipo_contrato','contrato_doc', 'fech_inicio_contrato', 'fech_fin_contrato']
+    
+    def __init__(self, *args, **kwargs):
+        super(ContratacionForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+        self.fields['fech_inicio_contrato'].widget.attrs.update({'id':'datetimepicker'})
+        self.fields['fech_fin_contrato'].widget.attrs.update({'id':'datetimepicker2'})
+
+class EducacionForm(BSModalForm):
+    class Meta:
+        model = Educacion
+        fields = ['empleado','nom_instituto', 'niv_educativo', 'deta_educacion', 'cursa_educacion', 'fech_inicio_edu', 'fech_fin_edu']
+
+    def __init__(self, *args, **kwargs):
+        super(EducacionForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+        self.fields['fech_inicio_edu'].widget.attrs.update({'id':'datetimepicker'})
+        self.fields['fech_fin_edu'].widget.attrs.update({'id':'datetimepicker2'})
+
+class Experiencia_LaboralForm(BSModalForm):
+    class Meta:
+        model = Experiencia_Laboral
+        fields = ['empleado','nom_empresa', 'cargo_experiencia', 'dura_experiencia','certificacion_labo','fech_inicio_expe', 'fech_fin_expe']
+    
+    def __init__(self, *args, **kwargs):
+        super(Experiencia_LaboralForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+        self.fields['fech_inicio_expe'].widget.attrs.update({'id':'datetimepicker'})
+        self.fields['fech_fin_expe'].widget.attrs.update({'id':'datetimepicker2'})
+
+class Documentos_SoporteForm(BSModalForm):
+    class Meta:
+        model = Documentos_Soporte
+        fields = ['empleado','tipo_documento','documento_soporte']
+
+    def __init__(self, *args, **kwargs):
+        super(Documentos_SoporteForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+
+class ValoracionForm(BSModalForm):
+    class Meta:
+        model = Valoracion
+        fields = ['empleado','valoracion', 'observacion']
+    
+    def __init__(self, *args, **kwargs):
+        super(ValoracionForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
